44af2f543f4b2bbae1842c7dd96d86b752ded5e1
Felix Ochoa <109564794+ochoafelix729@users.noreply.github.com>
2025-02-16 19:35:03 -0600

    Testing new code on virtual machine

---
+++ udp_handler.py
@@ -0,0 +1,37 @@
+import socket
+
+UDP_IP = "10.172.0.202" # I'm not entirely sure if this is the IP address we should be using
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def send_equipment_code(player_id, codename, team) -> bool:
+    """
+    Sends a player's ID, codename, and team to the UDP server
+    """
+
+    message = f"{team}, {player_id}, {codename}" # We can change this later if we want
+
+    try:
+        sock.sendto(message.encode(), (UDP_IP, UDP_PORT))
+        print(f"Sent: {message} to {UDP_IP}:{UDP_PORT}") # Same here
+        return True
+
+    except Exception as e:
+        print(f"Error sending UDP packet: {e}")
+        return False
+
+def set_server_ip(newip):
+    """
+    Sets UDP_IP to newip only if in correct format
+    """
+    global UDP_IP
+    UDP_IP = newip
+
+
+
+    
+
+
+
+
