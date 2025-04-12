fe593dd7b68e1c3e01c1fc5ad8ba5afeca62b401
Matthew Wilkinson <mww6121@gmail.com>
2025-04-12 01:05:54 -0500

    FUCK, PUT B TO THE LEFT PLEASEY

--- actionScreen.py
+++ actionScreen.py
@@ -259,9 +259,9 @@ class ActionScreen:
         }
 
         if teamTF:  # red team
-            tk.Entry(frame, textvariable=self.redScores[str(playerNum)][2], **base_style).pack(side=tk.RIGHT, padx=2)
+            tk.Entry(frame, textvariable=self.redScores[str(playerNum)][2], **base_style).pack(side=tk.LEFT, padx=2)
         else:  # green team
-            tk.Entry(frame, textvariable=self.greenScores[str(playerNum)][2], **base_style).pack(side=tk.RIGHT, padx=2)
+            tk.Entry(frame, textvariable=self.greenScores[str(playerNum)][2], **base_style).pack(side=tk.LEFT, padx=2)
 
         name = tk.Label(frame, text=playerCodename, bg=teamFrame["bg"], fg="white", font=("Arial", 12))
         name.pack(side=tk.LEFT, padx=2)
